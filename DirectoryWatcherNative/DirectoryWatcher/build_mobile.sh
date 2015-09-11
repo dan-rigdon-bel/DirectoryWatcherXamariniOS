8957a689e9a85cdaef35910d1f0f43f9376b1dca
Dan Belcher <dan@mcneel.com>
2015-09-11 15:39:28 -0700

    Initial Commit
    Ported from DirectoryWatcherTouch

---
+++ DirectoryWatcherNative/DirectoryWatcher/build_mobile.sh
@@ -0,0 +1,223 @@
+#!/bin/bash
+set -e
+
+has_xcodeProj=false;
+has_xcodeTools=true;
+has_lipo=true;
+is_ready_for_ios_build=false;
+did_build_ios_successfully=false;
+
+function xcodeProjCheck () {
+	printf " Checking for xcodeproj                "
+	if test ! -d DirectoryWatcher.xcodeproj;
+	then
+	  printf "...DirectoryWatcher.xcodeproj NOT FOUND\n"
+	  has_xcodeProj=false
+	fi
+
+	if test -d DirectoryWatcher.xcodeproj;
+	then
+		printf "...Ok\n"
+		has_xcodeProj=true
+	fi
+}
+
+function xcodeToolsCheck () {
+    printf " Checking for XCode command line tools "
+	XCCOMMANDS="xcodebuild"
+	for i in $XCCOMMANDS
+	do
+		command -v $i >/dev/null && continue || { printf "...$i NOT FOUND\n"; has_xcodeTools=false; }
+	done
+
+	if $has_xcodeTools
+	then
+		printf "...Ok\n"
+		has_xcodeTools=true;
+	fi
+}
+
+function lipoCheck () {
+	printf " Checking for lipo                     "
+	LIPOCOMMANDS="lipo"
+	for i in $LIPOCOMMANDS
+	do
+		command -v $i >/dev/null && continue || { printf "...$i NOT FOUND.\n"; has_lipo=false;}
+	done
+
+	if $has_lipo
+	then
+		printf "...Ok\n"
+	fi
+}
+
+function checkiOS () {
+	echo ""
+	echo "iOS Pre-build check---------------------------------------------"
+	xcodeProjCheck
+	xcodeToolsCheck
+	lipoCheck
+
+	if $has_xcodeProj && $has_xcodeTools && $has_lipo
+	then
+		is_ready_for_ios_build=true;
+		echo "STATUS: Ready to build libDirectoryWatcher.a for iOS"
+	fi
+
+	if ! $is_ready_for_ios_build
+	then
+		echo "STATUS: NOT ready for iOS build.  Please address the following:"
+		if ! $has_xcodeProj;
+		then
+			echo " ---ERROR: Script in wrong location or xcodeproj missing----------------"
+			echo "  This script must be placed in DirectoryWatcherNative/DirectoryWatcher folder."
+			echo "  This folder should contain the DirectoryWatcher.xcodeproj file that is used in "
+			echo "  the command line build.  If this script is in the proper folder,"
+			echo "  and you are getting this error message, then you are likely missing"
+			echo "  the XCode project.  Go to https://github.com/dbelcher/DirectoryWatcherTouch"
+			echo "  to download."
+		fi
+
+		if ! $has_xcodeTools
+		then
+			echo " ---ERROR: XCode Command Line Tools Missing-----------------------------"
+			echo "  Building the universal binary requires xcodebuild.  This utility"
+			echo "  is included with Apple's XCode Command Line Tools.  As of XCode 4.3,"
+			echo "  Command Line Tools are optional.  To install Command Line tools, open"
+			echo "  XCode > Preferences > Downloads Tab > Components > Command Line Tools."
+			echo "  Download and install.  Be sure to close/restart your terminal session"
+			echo "  before running this command again."
+		fi
+
+		if ! $has_lipo
+		then
+			echo " ---ERROR: Lipo missing or not in PATH----------------------------------"
+			echo "  Building the universal binary requires lipo.  If you are seeing this"
+			echo "  error, it is likely that lipo is not in your path.  Verify that lipo"
+			echo "  is in the /usr/bin folder and that this folder is in your PATH."
+		fi
+	fi
+}
+
+
+
+function createBuildFoldersForIOS () {
+	if test ! -d build
+	then
+		mkdir build
+	fi
+
+	#check to make sure the folder was created successfully
+	if test ! -d build
+	then
+		echo "ERROR: Unable to create build folders.  Please make sure you have admin privileges and try again."
+		exit 1;
+	fi
+
+	cd build
+
+	if test ! -d Release-ios
+	then
+		mkdir Release-ios
+	fi
+
+	#check to make sure the folder was created successfully
+	if test ! -d Release-ios
+	then
+		echo "ERROR: Unable to create build folders.  Please make sure you have admin privileges and try again."
+		exit 1;
+	fi
+
+	cd ..
+}
+
+function help () {
+	echo ""
+    echo "build_mobile.sh - bash shell script for building DirectoryWatcher for mobile platforms."
+    echo "usage: ./build_mobile.sh argument"
+    echo ""
+    echo "    argument:   description:"
+    echo ""
+	echo "    check       perform pre-requisites check for each platform"
+	echo "    help        display this screen"
+	echo ""
+}
+
+function iOSFinishedMessage () {
+	echo "STATUS: iOS Build Complete.  libDirectoryWatcher is in build/Release-ios"
+}
+
+
+function buildForiOS() {
+	echo "iOS Build-------------------------------------------------------"
+	echo "Making static libDirectoryWatcher.a for iOS..."
+
+	printf " Compiling i386 version                "
+	# add: > /dev/null  to suppress output on xcodeBuilds
+	xcodeBuild -project DirectoryWatcher.xcodeproj -target DirectoryWatcher -sdk iphonesimulator -configuration Release clean build
+	printf "...Done\n";
+	cd build/Release-iphonesimulator
+	mv libDirectoryWatcher.a libDirectoryWatcher-i386.a
+	mv libDirectoryWatcher-i386.a ../../build/Release-ios
+	cd ../..
+
+	printf " Compiling armv7 version               "
+	xcodeBuild -project DirectoryWatcher.xcodeproj -target DirectoryWatcher -sdk iphoneos -arch armv7 -configuration Release clean build
+	printf "...Done\n";
+	cd build/Release-iphoneos
+	mv libDirectoryWatcher.a libDirectoryWatcher-armv7.a
+	mv libDirectoryWatcher-armv7.a ../../build/Release-ios
+	cd ../..
+
+	printf " Compiling armv7s version              "
+	xcodeBuild -project DirectoryWatcher.xcodeproj -target DirectoryWatcher -sdk iphoneos -arch armv7s -configuration Release clean build
+	printf "...Done\n";
+	cd build/Release-iphoneos
+	mv libDirectoryWatcher.a libDirectoryWatcher-armv7s.a
+	mv libDirectoryWatcher-armv7s.a ../../build/Release-ios
+	cd ../..
+
+	printf " Compiling arm64 version               "
+	xcodeBuild -project DirectoryWatcher.xcodeproj -target DirectoryWatcher -sdk iphoneos -arch arm64 -configuration Release clean build
+	printf "...Done\n";
+	cd build/Release-iphoneos
+	mv libDirectoryWatcher.a libDirectoryWatcher-arm64.a
+	mv libDirectoryWatcher-arm64.a ../../build/Release-ios
+	cd ../..
+
+	printf " Creating Universal Binary             "
+	cd build/Release-ios
+	lipo -create -output libDirectoryWatcher.a libDirectoryWatcher-i386.a libDirectoryWatcher-armv7.a libDirectoryWatcher-armv7s.a libDirectoryWatcher-arm64.a
+	printf "...Done\n";
+	cd ../..
+
+	did_build_ios_successfully=true;
+}
+
+if [ "$1" = "help" ] || [ "$1" = "?" ]
+then
+    help
+elif [ "$1" = "check" ]
+then
+	checkiOS
+elif [ "$1" = "" ]
+then
+
+	checkiOS
+	createBuildFoldersForIOS
+
+	if $is_ready_for_ios_build;
+	then
+		buildForiOS
+	fi
+
+	if $did_build_ios_successfully;
+	then
+		iOSFinishedMessage
+	fi
+else
+    echo "ERROR: unknown argument.  build_mobile.sh help"
+    help
+fi
+
+exit 0
---
+++ README.md
@@ -0,0 +1,68 @@
+DirectoryWatcherXamariniOS
+==========================
+This small library allows you to watch for change events to a folder in your iOS Application's file system using C# and Xamarin.iOS.  At its core, DirectoryWatcherTouch is C# wrapper and Xamarin.iOS binding around the Objective-C [DirectoryWatcher](https://developer.apple.com/library/ios/samplecode/DocInteraction/Listings/Classes_DirectoryWatcher_h.html) class by Apple.  The FileSystemWatcherTouch class provides a subset of the functionality of the C# .NET [FileSystemWatcher](http://msdn.microsoft.com/en-us/library/System.IO.FileSystemWatcher) class.
+
+Pre-requisites
+--------------------
+You are going to need:
+
+* [Apple Xcode](http://developer.apple.com/xcode/)
+* [Xamarin.iOS](http://xamarin.com/ios)
+* [Xamarin Studio](http://xamarin.com/studio) or [Visual Studio Professional](http://msdn.microsoft.com/vstudio) with Xamarin extensions.
+
+Installation / Configuration
+----------------------------
+1.  Clone or download this repository to your Mac running OSX.  Place it in a convenient location such as _Development/Repositories/DirectoryWatcherXamariniOS_ or similar.
+2.  Build the native, static _libDirectoryWatcher.a_ library.  Open a terminal window and navigate to the _DirectoryWatcherXamariniOS/DirectoryWatcherNative/DirectoryWatcher_ folder.  (This folder contains a bash shell script for building the native library for the _iphonesimulator_, _armv7_, _armv7s_, and _arm64_.  These three architectures are bundled into single FAT binary using the lipo tool - see note below)  Simply run the following command at the prompt:
+
+	sudo ./build_mobile.sh
+
+3.  Once the static library has successfully built, you need to point the .NET binding project (_DirectoryWatcherXamariniOS_) to the correct location.  In [Xamarin Studio](http://xamarin.com/studio) or [Visual Studio](http://msdn.microsoft.com/vstudio), open the _DirectoryWatcherXamariniOS_ Solution contained in the _DirectoryWatcherXamariniOS/DirectoryWatcherXamariniOS_ folder.  The link to _libDirectoryWatcher.a_ may be broken and you will have to re-reference this file into the project.
+4.  Clean and Build the _DirectoryWatcherXamariniOS_ project.  The resulting _DirectoryWatcherXamariniOS.dll_ that can be referenced into your project.
+
+Usage
+----------------------------
+Use DirectoryWatcherXamariniOS...
+
+	using DirectoryWatcherXamariniOS;
+
+Create a property or backing field...
+
+	private FileSystemWatcherTouch DocumentsDirectoryWatcher { get; set; }
+
+Set up a FileSystemWatcher...
+
+	// Start watching the Documents folder...
+	string documentsFolder = Environment.GetFolderPath (Environment.SpecialFolder.MyDocuments);
+	DocumentsDirectoryWatcher = new FileSystemWatcherTouch (documentsFolder);
+	DocumentsDirectoryWatcher.Changed += OnDirectoryDidChange;
+
+Handle change event callbacks...
+
+	public void OnDirectoryDidChange (object sender, EventArgs args) {
+		Console.WriteLine ("Change detected in the Documents folder");
+		// Handle the change...
+	}
+
+Make sure you clean up after yourself when done...for example, in AppDelegate you might:
+
+	public override void WillTerminate (UIApplication application)
+	{
+		base.WillTerminate (application);
+
+		// Stop watching the Documents folder...
+		DocumentsDirectoryWatcher.Changed -= OnDirectoryDidChange;
+		DocumentsDirectoryWatcher.Dispose (); // This calls Invalidate() on the native watcher.
+	}
+
+Detailed Info
+----------------------------
+The native DirectoryWatcher class comes from this [Apple iOS Developer Library sample](https://developer.apple.com/library/ios/samplecode/DocInteraction/Listings/Classes_DirectoryWatcher_h.html).  This class uses the low-level _kqueue_ kernel event notification system.  The MonoTouch bindings wrapping this small library attempt (in a small way) to mimic Microsoft .NET [FileSystemWatcher](http://msdn.microsoft.com/en-us/library/System.IO.FileSystemWatcher) class, though only the _Changed_ event is current implemented.
+
+* Note on the Static Binary
+----------------------------
+The  build_mobile.sh build script included here builds a FAT binary targeting _iphonesimulator_, _armv7_, _armv7s_, and _arm64_.  To make the binary slightly smaller for release, you may remove the _iphonesimulator_ target.  Regardless, the FAT binary is quite small, so it may not be worth the effort.
+
+Authors
+-------
+* Dan Belcher - https://github.com/dbelcher dan@mcneel.com
